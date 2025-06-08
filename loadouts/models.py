1fbf6c90aa5ffd63738b0aae4d174c23e137cf20
Jan Klapetek <jan.klapetek@gmail.com>
2025-06-08 13:39:29 +0200

    import models

--- loadouts/models.py
+++ loadouts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib import admin
+from models import Weapon, Attachment, Loadout, Perk, Category
 
-# Create your models here.
+admin.site.register(Weapon)
+admin.site.register(Attachment)
+admin.site.register(Loadout)
+admin.site.register(Perk)
+admin.site.register(Category)
+
