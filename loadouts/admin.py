f31587537c65da3248c5e625b3c651c294c49a5d
Jan Klapetek <jan.klapetek@gmail.com>
2025-06-08 13:54:38 +0200

    added models def and class and admin.py imports

--- loadouts/admin.py
+++ loadouts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Weapon, Attachment, Loadout, Perk, Category
 
-# Register your models here.
+admin.site.register(Weapon)
+admin.site.register(Attachment)
+admin.site.register(Loadout)
+admin.site.register(Perk)
+admin.site.register(Category)
--- loadouts/models.py
+++ loadouts/models.py
@@ -1,10 +1,41 @@
 from django.db import models
-from django.contrib import admin
-from models import Weapon, Attachment, Loadout, Perk, Category
 
-admin.site.register(Weapon)
-admin.site.register(Attachment)
-admin.site.register(Loadout)
-admin.site.register(Perk)
-admin.site.register(Category)
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
+class Weapon(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Attachment(models.Model):
+    name = models.CharField(max_length=100)
+    weapon = models.ForeignKey(Weapon, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Perk(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Loadout(models.Model):
+    name = models.CharField(max_length=100)
+    weapon = models.ForeignKey(Weapon, on_delete=models.CASCADE)
+    attachments = models.ManyToManyField(Attachment, blank=True)
+    perks = models.ManyToManyField(Perk, blank=True)
+
+    def __str__(self):
+        return self.name
